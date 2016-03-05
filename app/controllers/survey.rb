045912fe78711fd6b9ebb59127a9338d75be15ab
Cody <email": null>
2016-03-05 15:44:53 -0800

    add survey information

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,1 +1,24 @@
-survey.rb
+get '/surveys' do
+
+end
+
+get '/surveys/new' do
+  erb :'surveys/new'
+end
+
+post '/surveys' do
+  Survey.create(survey_name: params[:survey_name])
+  Question.create(question_name: params[:question_name])
+end
+
+get '/surveys/:id' do
+end
+
+get 'surveys/:id/edit' do
+end
+
+put 'surveys/:id' do
+end
+
+delete 'survey/:id' do
+end
