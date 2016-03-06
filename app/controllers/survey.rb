64db1b84bcdb457972307139a1baed9c8b1f9244
tmashuang <tmashuang@gmail.com>
2016-03-05 20:18:15 -0800

    All this new shit

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,9 +1,8 @@
 get '/surveys' do
-  @surveyship = Surveyship.all
-	@surveys = Survey.all
+  @survey = Survey.all
+  @author = User.find_by(id: @survey.user_id)
 	erb :'/surveys/index'
-
-end
+end 
 
 get '/surveys/new' do
   erb :'surveys/new'
@@ -20,6 +19,8 @@ get '/surveys/:id' do
   @survey = Survey.find(params[:id])
   @question = Question.find_by(survey_id: @survey.id)
   @choice = Choice.find_by(question_id: @question.id)
+  @lame = Surveyship.find_by(author_id: @survey.user_id)
+  @shit = User.find_by(id: @lame.author_id)
   erb :'surveys/show'
 end
 
